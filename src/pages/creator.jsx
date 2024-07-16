5a0e5857a65125d84f9b6fa5c56d9d350e853674
Ramit Brata Biswas <ramitbratabiswas@gmail.com>
2024-07-16 12:10:49 -0700

    added grid layout

--- src/pages/creator.jsx
+++ src/pages/creator.jsx
@@ -1,16 +1,6 @@
-import { useFetchUniqueImages } from "../utils/fetchUniqueImages";
+import Banner from "../components/banner";
 
 export default function Creator() {
 
-  const imageLinks = useFetchUniqueImages();
-
-  return (
-    <div className='container'>
-      <div className='image-container'>
-        {imageLinks.map((link, index) => {
-          return <img className='image' id={`image-${index+1}`} src={link} key={index+1}/>;
-        })}
-      </div>
-    </div>
-  );
+  return <Banner />;
 }
