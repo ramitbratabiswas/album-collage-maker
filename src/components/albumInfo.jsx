04d23d3a49da5182b871ba32444d378a73424751
Ramit Brata Biswas <ramitbratabiswas@gmail.com>
2024-07-19 22:26:44 -0700

    added album search bar

--- src/components/albumInfo.jsx
+++ src/components/albumInfo.jsx
@@ -2,20 +2,22 @@ import { useContext } from "react";
 import { AppContext } from "../utils/appContext";
 
 export const AlbumInfo = () => {
-
   const { covers, selectedIndex } = useContext(AppContext);
-  if (covers[selectedIndex]) console.log(covers[selectedIndex]);
 
-  console.log("in AlbumInfo");
-  
   if (!covers[selectedIndex]) return null;
 
   return (
-    <div className='album-info'>
-      <h2>{covers[selectedIndex].album}</h2>
-      <p>{covers[selectedIndex].artist}</p>
-      <img className="album-info-image" src={covers[selectedIndex].link} alt={covers[selectedIndex].album} />
+    <div className={`album-info-sidebar ${covers[selectedIndex] ? 'open' : ''}`}>
+      <div className='album-info-card'>
+        <img className="album-info-image" src={covers[selectedIndex].link} alt={covers[selectedIndex].album} />
+        <p className="album-info-name">{covers[selectedIndex].album}</p>
+        <p className="album-info-artist-name">{covers[selectedIndex].artist}</p>
+      </div>
+      <p className="album-info-replace-with">replace with...</p>
+      <form className="album-info-search-form">
+        <input className="album-info-search" placeholder="type album name here..."></input>
+        <button className="album-info-search-submit" type="submit">Search</button>
+      </form>
     </div>
   );
-
 };
